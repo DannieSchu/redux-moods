4a7036238f51b456838828e9571e2063a64b8269
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-12 16:28:40 -0700

    write failing test for EAT_SNACK action

--- src/actions/moodActions.test.js
+++ src/actions/moodActions.test.js
@@ -1,4 +1,4 @@
-import { drinkCoffee, DRINK_COFFEE } from './moodActions';
+import { drinkCoffee, DRINK_COFFEE, eatSnack, EAT_SNACK } from './moodActions';
 
 describe('moods actions', () => {
   it('creates a DRINK_COFFEE action', () => {
@@ -6,6 +6,15 @@ describe('moods actions', () => {
 
     expect(action).toEqual({
       type: DRINK_COFFEE,
+      payload: 2
+    });
+  });
+
+  it('creates an EAT_SNACK action', () => {
+    const action = eatSnack(2);
+
+    expect(action).toEqual({
+      type: EAT_SNACK,
       payload: 2
     });
   });
