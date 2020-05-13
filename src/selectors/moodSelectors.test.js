726eceae1f731418e969b2e4e0c92c7701456b18
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-12 17:29:25 -0700

    getFace selector returns hyper face if over-caffeinated

--- src/selectors/moodSelectors.test.js
+++ src/selectors/moodSelectors.test.js
@@ -13,4 +13,17 @@ describe('mood selectors', () => {
 
     expect(mood).toEqual('🤬');
   });
+
+  it('returns hyper face if over-caffeinated', () => {
+    const state = {
+      coffees: 4,
+      snacks: 1,
+      naps: 0,
+      studies: 0
+    };
+
+    const mood = getFace(state);
+
+    expect(mood).toEqual('🙀');
+  });
 });
