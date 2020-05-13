69562c0c3e2e67535fb139042c6c7653b28805ed
Dannie Schumaker <draeschumaker@gmail.com>
2020-05-12 21:58:51 -0700

    import timer into Moods file

--- src/components/moods/Moods.js
+++ src/components/moods/Moods.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import Controls from '../controls/Controls';
 import Face from '../face/Face';
-import { useDispatch, useSelector } from 'react-redux';
+import Timer from '../timer/Timer';
 import { getFace } from '../../selectors/moodSelectors';
-import { drinkCoffee, eatSnack, takeNap, study } from '../../actions/moodActions';
 import { getCoffees, getSnacks, getNaps, getStudies } from '../../selectors/activitySelectors';
+import { drinkCoffee, eatSnack, takeNap, study } from '../../actions/moodActions';
 
-export default function Moods() {
+const ONE_MINUTE = 60;
+
+const Moods = () => {
   const dispatch = useDispatch();
   const coffees = useSelector(getCoffees);
   const snacks = useSelector(getSnacks);
   const naps = useSelector(getNaps);
   const studies = useSelector(getStudies);
   const face = useSelector(getFace);
+
+  const [time, setTime] = useState(ONE_MINUTE);
+  
+  useEffect(() => {
+    setInterval(() => {
+      if(time > 0) setTime(() => time - 1);
+    }, 1000);
+    if(time === 0) return <Redirect to="/" />;
+  });
 
   return (
     <>
@@ -23,6 +36,10 @@ export default function Moods() {
         <button onClick={() => dispatch(study())}>studies - {studies}</button>
       </Controls>
       <Face emoji={face} />
+      <Timer time={time} />
     </>
   );
-}
+};
+
+export default Moods;
+
